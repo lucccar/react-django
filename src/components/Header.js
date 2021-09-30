0e73279f55570076884dd9159f537bae98296334
Lucas Carvalho <lucas.crvlh.s@gmail.com>
2021-09-30 11:21:15 -0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+	appBar: {
+		borderBottom: `1px solid ${theme.palette.divider}`,
+	},
+}))
+
+function Header() {
+	const classes = useStyles();
+	return (
+		<React.Fragment>	
+			<CssBaseline />
+				<AppBar
+				position="static" color="white" elevation={0} className={classes.appBar}>
+					<Toolbar>
+						<Typography variant="h6" color="inherit" noWrap>
+							Blogmeup
+						</Typography>
+					</Toolbar>
+
+				</AppBar>
+		</React.Fragment>
+	);
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = <Router>
+    <React.StrictMode>
+        <Header />
+        <Switch>
+            <Route exact path="/" component={App} /> 
+        </Switch>
+
+        <Footer />
+    </React.StrictMode>
+</Router>
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
